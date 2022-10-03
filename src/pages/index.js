c82c6161c80eb68ffbc097173e0a9ff619e93c29
Utku İmdat <ut.imd@yandex.com>
2022-10-03 21:31:06 +0300

    Other main screens created

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,6 @@ export { default as Hello } from './auth/hello/Hello';
 export { default as GetUserInfo } from './auth/getUserInfo/GetUserInfo';
 export { default as GetPhoneNum } from './auth/getPhoneNum/GetPhoneNum';
 export { default as Verify } from './auth/verify/Verify';
-export { default as Home } from './main/home/Home';
+export { default as Messages } from './main/messages/Messages';
+export { default as Status } from './main/status/Status';
+export { default as Settings } from './main/settings/Settings';
---
+++ src/pages/main/status/Status.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './Status.style';
+
+const Status = () => {
+  return (
+    <View>
+      <Text>Status</Text>
+    </View>
+  )
+}
+
+export default Status;
