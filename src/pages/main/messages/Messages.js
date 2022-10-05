d5b4fe2541f1c966f2ad0d4f9f1f2d2ba4ddbfd2
Utku İmdat <ut.imd@yandex.com>
2022-10-05 16:58:18 +0300

    Messages screen updated

--- src/pages/main/messages/Messages.js
+++ src/pages/main/messages/Messages.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { Entypo } from '@expo/vector-icons';
 import styles from './Messages.style';
 
 const Messages = () => {
+  const navigation = useNavigation();
+  const { theme } = useSelector(state => state.theme);
+
+  const gotoContacts = () => {
+    navigation.navigate('ContactListScreen');
+  }
+  
   return (
-    <View>
-      <Text>Messages</Text>
-    </View>
+    <SafeAreaView style={[styles.container, {backgroundColor: theme.backgroundColor}]}>
+      <TouchableOpacity style={[styles.newMessage, {backgroundColor: theme.headerColor}]} onPress={gotoContacts}>
+        <Entypo name="new-message" size={28} color="white" />
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
--- src/pages/main/messages/Messages.style.js
+++ src/pages/main/messages/Messages.style.js
@@ -1,5 +1,16 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
-    
+    container: {
+        flex: 1,
+    },
+    newMessage: {
+        width: Dimensions.get('screen').width / 5,
+        height: Dimensions.get('screen').width / 5,
+        borderRadius: Dimensions.get('screen').width / 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: '80%',
+        left: '75%'
+    }
 })
