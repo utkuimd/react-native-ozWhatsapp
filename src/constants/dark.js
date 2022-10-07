7663a02e0d0980256e6d617f4935aeb11a8f3193
Utku İmdat <ut.imd@yandex.com>
2022-10-07 18:09:06 +0300

    New properties added to constants

--- src/constants/dark.js
+++ src/constants/dark.js
@@ -3,4 +3,9 @@ export default {
   backgroundColor: '#14202b',
   color: 'white',
   headerColor: '#131c2a',
+  textInputColor: 'black',
+  divColor: 'black',
+  backgroundImage: 'https://wallpaperaccess.com/full/2224386.jpg',
+  smsgDivColor: '#1F51FF',
+  rmsgDivColor: 'black'
 };
--- src/constants/light.js
+++ src/constants/light.js
@@ -3,4 +3,9 @@ export default {
   backgroundColor: '#fffafa', // snow / maybe try another color #fffff0 - ivory
   color: 'black',
   headerColor: 'orange',
+  textInputColor: 'snow',
+  divColor: 'orange',
+  backgroundImage: 'https://i.pinimg.com/originals/bc/da/1d/bcda1d0c891d235fbf680a893265d68f.jpg',
+  smsgDivColor: 'limegreen',
+  rmsgDivColor: 'white'
 };
